1cce838726880fc9a9df5bb3fafcbb35d3ab86cf
PatriciaGonFu <patruskatrus@hotmail.com>
2023-11-17 11:30:13 +0100

    cambios

---
+++ MaquetaciónBásica/RetoAgenciaDeViajes/agenciaViajes.js
@@ -0,0 +1,20 @@
+class Persona {
+    constructor(nombre, apellido, email) {
+      this.nombre = Nombre;
+      this.apellidos = Apellidos;
+      this.telefono = Telefono;
+      this.correoElectronico = CorreoElectronico;
+      this.origen = Origen;
+      this.destino = Destino;
+      this.fechaDeSalida = FechaDeSalida
+    }
+  }
+  
+  function agregarPersona() {
+    const nombre = document.getElementById('nombre').value;
+    const apellidos = document.getElementById('apellidos').value;
+    const correoElectronico = document.getElementById('email').value;
+    const.
+  }
+    const nuevaPersona = new Persona(nombre, apellido, email);
+
