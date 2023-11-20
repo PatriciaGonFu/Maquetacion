41be7a7f481bb28498d00e49cce2ae44b07b1715
PatriciaGonFu <patruskatrus@hotmail.com>
2023-11-20 11:28:28 +0100

    reto terminado

--- MaquetaciónBásica/RetoAgenciaDeViajes/agenciaViajes.js
+++ MaquetaciónBásica/RetoAgenciaDeViajes/agenciaViajes.js
@@ -1,20 +1,47 @@
-class Persona {
-    constructor(nombre, apellido, email) {
-      this.nombre = Nombre;
-      this.apellidos = Apellidos;
-      this.telefono = Telefono;
-      this.correoElectronico = CorreoElectronico;
-      this.origen = Origen;
-      this.destino = Destino;
-      this.fechaDeSalida = FechaDeSalida
+let data = [];
+
+function obtenerDatos() {
+  let nombreInfo = document.getElementById("name").value;
+  let origenInfo = document.getElementById("origen").value;
+  let destinoInfo = document.getElementById("destino").value;
+  let personasInfo = document.getElementById("personas").value;
+  let fechaInfo = document.getElementById("fecha").value;
+  
+  let usuarios = {
+    nombre: nombreInfo,
+    origen: origenInfo,
+    destino: destinoInfo,
+    personas: personasInfo,
+    fecha: fechaInfo
+  };
+
+data.push(usuarios)
+console.log(data);
+
+}
+
+
+function filtrarDatos(){
+  let filtrado = "";
+
+  for(let i=0;i<data.length;i++){
+
+    if (data[i].destino.toUpperCase() == "CANARIAS" || data[i].destino.toUpperCase() == "MALLORCA"
+    || data[i].destino.toUpperCase() == "GALICIA"){
+
+      console.log(data[i]);
+      
+      filtrado += `<p> Nombre: ${data[i].nombre}</p>
+      <p> Origen: ${data[i].origen}</p>
+      <p> Destino: ${data[i].destino}</p>
+      <p> Nº Personas: ${data[i].personas}</p>
+      <p> Fecha: ${data[i].fecha}</p>`
     }
   }
-  
-  function agregarPersona() {
-    const nombre = document.getElementById('nombre').value;
-    const apellidos = document.getElementById('apellidos').value;
-    const correoElectronico = document.getElementById('email').value;
-    const.
-  }
-    const nuevaPersona = new Persona(nombre, apellido, email);
+
+  document.getElementById('obtenerDatos').innerHTML= `${filtrado}`;
+}
+
+
+
 
